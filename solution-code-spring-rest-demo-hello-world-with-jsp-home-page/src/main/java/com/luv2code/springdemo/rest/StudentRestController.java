e26dd71368b66ccca0925c9f0f1e987307ecaed6
Ke Yuan <yubaoxiaolin@gmail.com>
2023-02-18 17:46:57 +0200

    spring REST pathVariable

--- solution-code-spring-rest-demo-hello-world-with-jsp-home-page/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ solution-code-spring-rest-demo-hello-world-with-jsp-home-page/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,8 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +15,33 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	//define @PostConstructor to load data..
+	
+	@PostConstruct
+	public void loadData() {
+		
+		theStudents = new ArrayList<>();
+
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
 
 	// define endpoint for "/students" - return list of students
 	
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-
-		List<Student> theStudents = new ArrayList<>();
-		
-		theStudents.add(new Student("Poornima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));		
+	@GetMapping("/students/")
+	public List<Student> getStudent(){		
 			
 		return theStudents;
+	}
+	// define endpoint for "/students/{studentId}"
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return theStudents.get(studentId);
 	}
 	
 }
